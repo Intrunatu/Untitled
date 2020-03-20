5380f78a5a826ae6f153a419bfd5fdd39f356bba
Jean-Laurent Duchaud <duchaud@crans.org>
2020-03-20 11:47:43 +0100

    Utilise un fillgaps avant de calculer les resultats

--- code/forecast_and_plot.m
+++ code/forecast_and_plot.m
@@ -32,6 +32,44 @@ clearvars rawData
 
 
 
+
+% Options du modele
+solisOpts.phi      = 42.497;   % latitude degre
+solisOpts.lambda   =  2.030;   % longitude degre
+solisOpts.altitude = 1650;     % altitude en m
+solisOpts.zone     = 2;        % type d'aerosol 1=rural 2=maritime 3=urban 4=tropospherique
+solisOpts.azimut   = 1.63;     % azimut en degré
+solisOpts.albedo   = 0.25;     % albédo du sol
+solisOpts.tilt     = 30;       % angle d'inclinaison en degre
+solisOpts.oad      = 0.2;      % prof optique pour aerosol a 700nm
+solisOpts.w        = 1.8;      % colonne d'eau en cm
+opts.solisOpts=solisOpts;
+
+
+dt = 60;
+disp([dt 6*60/dt])
+
+opts.timeStep = dt;
+opts.sunHeightLim = 5;
+
+opts.Nhist = 12;
+opts.Npred = ceil(6*60/dt);
+opts.Nskip = 0;
+
+
+rng(1)
+[fm, inputTableForecast] = forecastModel(inputTableForecast, 'ARMA', opts,...
+    'plot'                  , false     , ...
+    'fillGaps'              , true      , ...
+    'gapInterpolationLimit' , 5         , ...
+    'gapPersistenceLimit'   , 30        , ...  % n'utilise pas la persistance
+    'gapClearskyLimit'      , 30        , ...
+    'nightBehaviour'        , 'deleteNightValues' , ...
+    'verbose'               , false);
+
+
+
+
 results = cell(size(fmList));
 for i =1:length(fmList)
     disp(i)
@@ -68,8 +106,6 @@ figure(3)
 x = results{1}(:,2);
 y = results{1}(1,1);
 z = results{1}(:,3);
-
-
 for i=2:length(results)
     r= results{i};
     dt = r(:,1);
@@ -79,7 +115,6 @@ for i=2:length(results)
     
     z = [z interp1(t,rmse,x)];
 end
-
 surf(x,y,z')
 zlabel('nRMSE')
 xlabel('Horizon')
