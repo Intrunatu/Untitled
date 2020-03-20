fa8ea5cf04757e9d6b8b8b497c78ff5624f3e3b8
Jean-Laurent Duchaud <duchaud@crans.org>
2020-03-20 10:38:26 +0100

    Script pour entrainer les modeles

---
+++ code/prepare_table_and_models.m
@@ -0,0 +1,53 @@
+clear all; close all; clc
+addpath([userpath '\PartageDeCode\toolbox\'])
+addpath([userpath '\PartageDeCode\toolbox\sources\prevision\'])
+
+% Données météo
+rawData = load([userpath '\Data\Données odeillo\Odeillo_UTC.mat']);
+inputTable = timetable(rawData.table.TimeUTC, rawData.table.GHI);
+clearvars rawData
+inputTable.Properties.VariableNames={'Irradiance'};
+inputTable(isnat(inputTable.Time),:)=[];
+inputTable(inputTable.Irradiance<0,:) = [];
+
+inputTableTrain=inputTable(5113147:5638657,:); % Du 01/01/14 au 01/01/15
+
+
+
+% Options du modele
+solisOpts.phi      = 42.497;   % latitude degre
+solisOpts.lambda   =  2.030;   % longitude degre
+solisOpts.altitude = 1650;     % altitude en m
+solisOpts.zone     = 2;        % type d'aerosol 1=rural 2=maritime 3=urban 4=tropospherique
+solisOpts.azimut   = 1.63;     % azimut en degré
+solisOpts.albedo   = 0.25;     % albédo du sol
+solisOpts.tilt     = 30;       % angle d'inclinaison en degre
+solisOpts.oad      = 0.2;      % prof optique pour aerosol a 700nm
+solisOpts.w        = 1.8;      % colonne d'eau en cm
+opts.solisOpts=solisOpts;
+
+for i = 1:3
+    dt = 20*i;
+    disp([dt 6*60/dt])
+    
+    opts.timeStep = dt;
+    opts.sunHeightLim = 5;
+    
+    opts.Nhist = 12;
+    opts.Npred = ceil(6*60/dt);
+    opts.Nskip = 0;
+    
+    tic
+    rng(1)
+    [fm, filledTableTrain] = forecastModel(inputTableTrain, 'ARMA', opts,...
+        'plot'                  , false     , ...
+        'fillGaps'              , true      , ...
+        'gapInterpolationLimit' , 5         , ...
+        'gapPersistenceLimit'   , 30        , ...  % n'utilise pas la persistance
+        'gapClearskyLimit'      , 30        , ...
+        'nightBehaviour'        , 'deleteNightValues' , ...
+        'verbose'               , false);
+    inputTableTrain = filledTableTrain;
+    fm.save(sprintf('fmARMA_6h_%02.0fmin', dt));
+    toc
+end
