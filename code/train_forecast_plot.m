22486166303b98d3d402a68330cffd090af8085b
Jean-Laurent Duchaud <duchaud@crans.org>
2020-03-25 12:07:59 +0100

    Refactorisation du code prepare/train
    Fait les fillgaps dans une 1ere fonction et le reste dans une 2de

--- code/train_forecast_plot.m
+++ code/train_forecast_plot.m
@@ -1,10 +1,46 @@
 clear all; close all; clc
 addpath([userpath '\PartageDeCode\toolbox\'])
 addpath([userpath '\PartageDeCode\toolbox\sources\prevision\'])
-load('inputTableForecast')
+load('filledTables')
 
-data = load("fmArray_ARMA_6h.mat", 'fm');
-fmList = data.fm;
+
+% Options du modele
+solisOpts.phi      = 42.497;   % latitude degre
+solisOpts.lambda   =  2.030;   % longitude degre
+solisOpts.altitude = 1650;     % altitude en m
+solisOpts.zone     = 2;        % type d'aerosol 1=rural 2=maritime 3=urban 4=tropospherique
+solisOpts.azimut   = 1.63;     % azimut en degré
+solisOpts.albedo   = 0.25;     % albédo du sol
+solisOpts.tilt     = 30;       % angle d'inclinaison en degre
+solisOpts.oad      = 0.2;      % prof optique pour aerosol a 700nm
+solisOpts.w        = 1.8;      % colonne d'eau en cm
+opts.solisOpts=solisOpts;
+
+%% Entraine les modèles
+for i = 1:12
+    dt = 5*i;
+    disp([dt 6*60/dt])
+    
+    opts.timeStep = dt;
+    opts.sunHeightLim = 5;
+    
+    opts.Nhist = ceil(6*60/dt);
+    opts.Npred = ceil(6*60/dt);
+    opts.Nskip = 0;
+    
+    tic
+    rng(1)
+    fmList(i) = forecastModel(filledTableTrain, 'ARMA', opts,...
+        'plot'                  , false     , ...
+        'fillGaps'              , true      , ...
+        'gapInterpolationLimit' , 5         , ...
+        'gapPersistenceLimit'   , 30        , ...  % n'utilise pas la persistance
+        'gapClearskyLimit'      , 30        , ...
+        'nightBehaviour'        , 'deleteNightValues' , ...
+        'verbose'               , false);
+    toc
+end
+save("fmArray_ARMA_6h", 'fmList')
 
 %% Metrics inside
 figure(1)
@@ -17,8 +53,8 @@ for i =1:length(fmList)
     plot3(t, dt, metrics_inside{6,2:end}), hold all
 end
 
-%% Metrics outide
 
+%% Metrics outide
 results = cell(size(fmList));
 for i =1:length(fmList)
     disp(i)
@@ -28,7 +64,7 @@ for i =1:length(fmList)
     fm.cleanPara.enable = false;
    
     % Calcul complet pour faire les erreurs à la main. Long à cause du fillGaps
-    [timePred, GiPred, GiMeas, isFilled, avgTable] = fm.forecast_full(inputTableForecast);
+    [timePred, GiPred, GiMeas, isFilled, avgTable] = fm.forecast_full(filledTableForecast);
     GiMeas(isFilled) = NaN;
     GiPred(isFilled) = NaN;
     metrics = fm.get_metrics(GiMeas, GiPred);   
