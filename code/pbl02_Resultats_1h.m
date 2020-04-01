ae1c07e9387afe1acaf3deee03f10123739ec7a3
Jean-Laurent Duchaud <duchaud@crans.org>
2020-04-01 13:02:59 +0200

    Calcul avec les 5 modeles

--- code/pbl02_Resultats_1h.m
+++ code/pbl02_Resultats_1h.m
@@ -1,14 +1,26 @@
-%% Resultats
+%% Resultats tout modèles 2h
+% Même objectif que précédemment mais avec tout les modèles. Ceratains
+% prennent trop de temps (RF et GP) donc j'ai réduit à 2h pour l'horizon
+% max et 10min pour le pas de temps mini.
+%
+% Options des modèles : 
+% 
+% * 2h historique
+% * 2h predictions
+% * pas de saut avant 1ere mesure
+% * Lignes contenant une nuit ($h>5^\circ$) supprimées
+%
+% 
 
+%%%
 clear all; close all; clc
 addpath([userpath '\PartageDeCode\toolbox\'])
 addpath([userpath '\PartageDeCode\toolbox\sources\prevision\'])
 load('filledTables')
 
 
-timeSteps = [ 10:5:30];
-% modelList = {'ARMA', 'NN', 'RF', 'SVR', 'GP'};
-modelList = {'ARMA', 'NN', 'SVR'};
+timeSteps = 10:5:30;
+modelList = {'ARMA', 'NN', 'RF', 'SVR', 'GP'};
 maxHoriz = 2*60;
 
 %% Calculs
@@ -35,6 +47,34 @@ if false
 end
 
 %% Affichage
+load('pbl02_results.mat')
+figure(2), clf
+col = lines;
+for iTS = 1:size(metrics,2)
+    for iModel = 1:size(metrics,1)
+        m = metrics{iModel,iTS};
+        steps = timeSteps(iTS)*(1:ceil(maxHoriz/timeSteps(iTS)));
+        
+        h(iModel, iTS) = plot3(steps,...
+            repmat(timeSteps(iTS),length(steps),1),...
+            m{6,2:end}*100, '.-', ...
+            'DisplayName', modelList{iModel},...
+            'Color', col(iModel,:));
+        hold all
+    end
+end
+grid on
+legend(modelList)
+xlabel('Horizon [min]')
+ylabel('TimeStep [min]')
+zlabel('nRMSE [%]')
+xticks(0:30:max(xlim))
+
+snapnow;
+set(h(2,:), 'Visible', 'off')
+set(h(5,:), 'Visible', 'off')
+
+%%%
 % Le modèle SVR est meilleur que les autres. Il faut que je modifie le code
 % d'entrainement des modèles parce que pour le moment, deux modèles qui
 % ont le même pas de temps mais pas le meme horizon max n'ont pas les mêmes
@@ -42,24 +82,3 @@ end
 % figure que ARMA et NN restent proches, même au pas de temps 10min. Sur la
 % figure de pbl01 (calculé avec un horizon max de 6h) le NN est à l'ouest
 % comparé au ARMA.
-load('pbl02_results.mat')
-figure(2), clf
-col = lines;
-for iTS = 1:size(fmList,2)
-    for iModel = 1:size(fmList,1)
-        m = metrics{iModel,iTS};
-        steps = timeSteps(iTS)*(1:ceil(maxHoriz/timeSteps(iTS)));
-        
-        plot3(steps,...
-            repmat(timeSteps(iTS),length(steps),1),...
-            m{6,2:end}*100, '.-', ...
-            'DisplayName', modelList{iModel},...
-            'Color', col(iModel,:))
-        hold all
-    end
-end
-grid on
-xlabel('Horizon [min]')
-ylabel('TimeStep [min]')
-zlabel('nRMSE [%]')
-xticks(0:30:max(xlim))
