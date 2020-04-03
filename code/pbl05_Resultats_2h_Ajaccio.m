248840550ad6af50de8b98645a68c192af0433ad
Jean-Laurent Duchaud <duchaud@crans.org>
2020-04-03 11:31:36 +0200

    Ajout Ajaccio ARMA 6h

--- code/pbl05_Resultats_2h_Ajaccio.m
+++ code/pbl05_Resultats_2h_Ajaccio.m
@@ -3,14 +3,14 @@
 % prennent trop de temps (RF et GP) donc j'ai réduit à 2h pour l'horizon
 % max et 10min pour le pas de temps mini.
 %
-% Options des modèles : 
-% 
+% Options des modèles :
+%
 % * 2h historique
 % * 2h predictions
 % * pas de saut avant 1ere mesure
 % * Lignes contenant une nuit ($h>5^\circ$) supprimées
 %
-% 
+%
 
 %%%
 clear all; close all; clc
@@ -28,7 +28,7 @@ maxHoriz = 2*60;
 % sauvegardés. Pour RF et GP, cela prend trop de place sur le disque
 % (plusieurs Go !!!) donc ils sautent.
 if false
-    for i = 1:length(modelList)        
+    for i = 1:length(modelList)
         fmList(i,:) = train_models(filledTableTrain, modelList{i}, fm1, timeSteps, maxHoriz);
     end
     
@@ -43,11 +43,11 @@ if false
         GiPred(isFilled) = NaN;
         metrics{i} = fm.get_metrics(GiMeas, GiPred);
     end
-    save('pbl02_results_AJO.mat', 'metrics')
+    save('pbl05_results_AJO.mat', 'metrics')
 end
 
 %% Affichage
-load('pbl02_results_AJO.mat')
+load('pbl05_results_AJO.mat')
 figure(2), clf
 col = lines;
 for iTS = 1:size(metrics,2)
@@ -75,3 +75,32 @@ set(h(2,:), 'Visible', 'off')
 set(h(5,:), 'Visible', 'off')
 
 %%%
+
+
+
+%% Ajaccio 6h
+if false
+    fmList = train_models(filledTableTrain, 'ARMA', fm1, 5:5:60, 6*60);
+    save(sprintf("fmArray_%s_6h_AJO", fmList(1).modelType), 'fmList')
+    
+    results = calc_results(fmList,filledTableForecast);
+    save(sprintf("results_%s_6h_AJO", fmList(1).modelType), 'results')
+else
+    load('results_ARMA_6h_AJO')
+end
+
+% Affichage en ligne
+figure(1), clf
+for i = 1:length(results)
+    r  = results{i};    % Matrice des resultats
+    dt = r(:,1);        % Timestep
+    t  = r(:,2);        % Horizon de pred
+    rmse = r(:,3);      % Erreur
+    plot3(t, dt, rmse, '.-'), hold all
+end
+zlabel('nRMSE')
+xlabel('Horizon')
+xticks(0:60:max(xlim))
+ylabel('TimeStep')
+zlim([0 0.5])
+grid on
