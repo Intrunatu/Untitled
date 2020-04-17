9e409d293de2a4cf96d0db0d53722bcf62ca46b5
Jean-Laurent Duchaud <duchaud@crans.org>
2020-04-17 20:01:30 +0200

    Ajout option masquer code

--- code/publish_all.m
+++ code/publish_all.m
@@ -1,7 +1,12 @@
 %% Republie tout les fichiers
 filelist = dir("pbl*.m");
+[~,repoPath] = system('git rev-parse --show-toplevel');
+xslPath = fullfile(repoPath(1:end-1),'mxdom2simplehtml.xsl');
+delete('html/*')
+
+
 for i =1:length(filelist)
-    publish(filelist(i).name);
+    publish(filelist(i).name, 'stylesheet', xslPath );
 end
 
 
@@ -17,3 +22,31 @@ fclose(fileID);
 publish('index');
 delete('index.m');
 zip('html','html/*');
+
+
+
+
+
+% <!------------------------------------------------------------------------------------->
+% <script>
+% function hide_all(){
+%   list = document.getElementsByClassName('codeinput');
+%   for (let i = 0; i < list.length; i++) {
+%     list[i].style.display = 'none';
+%   }
+%   document.getElementById("toggleCode").onclick = show_all; 
+% }
+% 
+% function show_all(){
+%   list = document.getElementsByClassName('codeinput');
+%   for (let i = 0; i < list.length; i++) {
+%     list[i].style.display = 'block';
+%   }
+%   document.getElementById("toggleCode").onclick = hide_all; 
+% }
+% 
+% window.onload = hide_all;  
+% </script>
+% 
+% <p><a id="toggleCode" onclick = "hide_all()">Afficher / Masquer le code</a></p>
+% <!------------------------------------------------------------------------------------->
